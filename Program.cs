a1195051b1fa18f180f379366e9a037cd73f6f90
YacineTrousselle <yacinetrousselle@gmail.com>
2024-03-23 18:53:49 +0100

    use config file

--- Program.cs
+++ Program.cs
@@ -6,81 +6,76 @@ using Microsoft.Extensions.Configuration;
 using Exception = System.Exception;
 using FileSenderI = FirstServer.iceImpl.Soup.FileSenderI;
 
-namespace FirstServer
+namespace FirstServer;
+
+public class ApplicationI : Application
 {
-    public class ApplicationI : Application
+    private MongoDbService _client;
+
+    public ApplicationI(MongoDbService client)
     {
-        private MongoDbService _client;
-
-        public ApplicationI(MongoDbService client)
-        {
-            _client = client;
-        }
-
-        public override int run(string[] args)
-        {
-            try
-            {
-                Core.Initialize();
-                using (Communicator communicator = Application.communicator())
-                {
-                    var adapter =
-                        communicator.createObjectAdapterWithEndpoints("SoupAdapter",
-                            $"default -h {args[0]} -p {args[1]}");
-                    InitSoup(adapter, _client);
-                    adapter.activate();
-                    Console.Out.WriteLine("Server is running...");
-                    communicator.waitForShutdown();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.Error.WriteLine(e);
-                return 1;
-            }
-
-            return 0;
-        }
-        
-        private void InitSoup(ObjectAdapter adapter, MongoDbService mongoDbService)
-        {
-            adapter.add(new FileUploaderI(mongoDbService), Util.stringToIdentity("Soup.FileUploader"));
-            adapter.add(new FileSenderI(mongoDbService), Util.stringToIdentity("Soup.FileSender"));
-            adapter.add(new SongDataModuleI(mongoDbService), Util.stringToIdentity("Soup.SongDataModule"));
-            adapter.add(new AudioPlayerI(mongoDbService), Util.stringToIdentity("Soup.AudioPlayer"));
-        }
+        _client = client;
     }
 
-    public class Program
+    public override int run(string[] args)
     {
-        public static string SongPath;
-        
-        public static int Main(string[] args)
+        try
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("settings.json", optional: false, reloadOnChange: true);
-            IConfiguration configuration = builder.Build().GetSection("Properties");
-            CheckConfig(configuration);
-            
-            var client = new MongoDbService(configuration["databaseUrl"]);
-            SongPath = configuration["songsPath"];
-            
-            Application application = new ApplicationI(client);
-
-            return application.main([
-                configuration["url"],
-                configuration["port"],
-            ]);
+            Core.Initialize();
+            using (Communicator communicator = Application.communicator())
+            {
+                var adapter =
+                    communicator.createObjectAdapter("SoupAdapter");
+                InitSoup(adapter, _client);
+                adapter.activate();
+                Console.Out.WriteLine("Server is running...");
+                communicator.waitForShutdown();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.Error.WriteLine(e);
+            return 1;
         }
 
-        private static void CheckConfig(IConfiguration configuration)
+        return 0;
+    }
+        
+    private void InitSoup(ObjectAdapter adapter, MongoDbService mongoDbService)
+    {
+        adapter.add(new FileUploaderI(mongoDbService), Util.stringToIdentity("Soup.FileUploader"));
+        adapter.add(new FileSenderI(mongoDbService), Util.stringToIdentity("Soup.FileSender"));
+        adapter.add(new SongDataModuleI(mongoDbService), Util.stringToIdentity("Soup.SongDataModule"));
+        adapter.add(new AudioPlayerI(mongoDbService), Util.stringToIdentity("Soup.AudioPlayer"));
+    }
+}
+
+public class Program
+{
+    public static string SongPath;
+        
+    public static int Main(string[] args)
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("settings.json", optional: false, reloadOnChange: true);
+        IConfiguration configuration = builder.Build().GetSection("Properties");
+        CheckConfig(configuration);
+            
+        var client = new MongoDbService(configuration["databaseUrl"]);
+        SongPath = configuration["songsPath"];
+            
+        Application application = new ApplicationI(client);
+
+        return application.main(args);
+    }
+
+    private static void CheckConfig(IConfiguration configuration)
+    {
+        string[] necessaryKeys = ["databaseUrl", "songsPath"];
+        foreach (var necessaryKey in necessaryKeys)
         {
-            string[] necessaryKeys = ["url", "port", "databaseUrl", "songsPath"];
-            foreach (var necessaryKey in necessaryKeys)
-            {
-                ArgumentNullException.ThrowIfNull(configuration[necessaryKey]);
-            }
+            ArgumentNullException.ThrowIfNull(configuration[necessaryKey]);
         }
     }
 }
